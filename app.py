365538a9cd8b8286cbe3d2bff4e6ebb2d19e6a31
bhavyaboilla <boillabhavya75@gmail.com>
2026-07-01 23:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-st.title("🎓 AI Learning Buddy")
+st.title("🎓 AI Learning Buddy boilla bhavya")
 
 topic = st.text_input("Enter a topic")
 
